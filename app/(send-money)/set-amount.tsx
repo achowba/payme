62b6de19292f5123b3c2ec15a33e2f3f91766270
Prince <justprince9@gmail.com>
2025-09-21 12:08:38 +0400

    refactor(set-amount): refactored transfer info UI into TransferInfo component
    - Extracted the wallet balance and selected contact display from set-amount.tsx into a new reusable TransferInfo component.
    - Updated set-amount.tsx to use TransferInfo, simplifying the main screen and improving code organization.

--- app/(send-money)/set-amount.tsx
+++ app/(send-money)/set-amount.tsx
@@ -1,12 +1,9 @@
-import WalletBalanceText from '@/components/home/WalletBalanceText';
+import TransferInfo from '@/components/send-money/TransferInfo';
 import { NumericKeypad } from '@/components/ui/NumericKeypad';
-import { WALLET_PATTERNS_BG } from '@/constants/assets.constants';
 import { COLORS } from '@/constants/colors.constants';
 import { DEFAULT_STYLES } from '@/constants/styles.constants';
 import { useSelectContact } from '@/hooks/useContactSelect';
-import { resolveSource } from '@/utils/assets.utils';
 import { sanitizeAmount } from '@/utils/number.utils';
-import { Image, ImageBackground } from 'expo-image';
 import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
 import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
@@ -17,11 +14,7 @@ const SetAmountScreen = () => {
   const [amount, setAmount] = useState('');
   const { selectedContact } = useSelectContact();
 
-  const changeContact = () => {
-    router.back();
-  };
-
-  const handleNumPadPress = (digit: string) => {
+  const onNumericKeypadPress = (digit: string) => {
     if (digit === 'delete') {
       setAmount((prev: string) => prev.slice(0, -1));
       return;
@@ -32,52 +25,37 @@ const SetAmountScreen = () => {
     });
   };
 
+  const onSendMoney = () => {
+    const numericAmount = parseFloat(amount);
+
+    if (isNaN(numericAmount) || numericAmount <= 0) {
+      alert('Please enter a valid amount greater than zero.');
+      return;
+    }
+
+    router.push('/sending');
+  };
+
   return (
     <View style={styles.container}>
       <View>
-        <View style={styles.infoContainer}>
-          <ImageBackground style={styles.balanceContainer} source={WALLET_PATTERNS_BG}>
-            <Text style={[styles.text, styles.balanceLabelText]}>Wallet Balance</Text>
-            <WalletBalanceText balance={12890.0} />
-          </ImageBackground>
-          <View style={styles.selectedContactContainer}>
-            <View style={styles.selectedContactInfoContainer}>
-              <View style={styles.imageContainer}>
-                <Image source={resolveSource(selectedContact?.photo)} style={styles.image} />
-              </View>
-              <View>
-                <Text style={[styles.text, styles.selectedContactNameText]}>
-                  {selectedContact?.firstName} {selectedContact?.lastName}
-                </Text>
-                <Text style={[styles.text, styles.selectedContactCardInfoText]}>
-                  Payme - **** **** **** 9839
-                </Text>
-              </View>
-            </View>
-            <Pressable
-              style={({ pressed }) => pressed && DEFAULT_STYLES.pressed}
-              onPress={changeContact}
-            >
-              <Text style={styles.changeContactBtnText}>Change</Text>
-            </Pressable>
-          </View>
-        </View>
+        <TransferInfo selectedContact={selectedContact} showChangeBtn />
         <View style={styles.amountInputContainer}>
           <Text style={styles.currencyText}>$</Text>
           <TextInput
             style={styles.amountInputField}
             value={amount}
-            // onChangeText={handleAmountInputChange}
             keyboardType="number-pad"
             selectionColor={COLORS.dark.yellow}
           />
         </View>
         <View style={styles.numPadContainer}>
-          <NumericKeypad onPress={handleNumPadPress} />
+          <NumericKeypad onPress={onNumericKeypadPress} />
         </View>
       </View>
       <View style={styles.sendMoneyBtnContainer}>
         <Pressable
+          onPress={onSendMoney}
           style={({ pressed }) => [styles.sendMoneyBtn, pressed && DEFAULT_STYLES.pressed]}
         >
           <Text style={styles.sendMoneyBtnText}>Send Money</Text>
@@ -91,65 +69,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 2,
-  },
-  infoContainer: {
-    backgroundColor: COLORS.dark.secondary,
-    borderRadius: 30,
-    marginBottom: 20,
-    padding: 10,
-  },
-  balanceContainer: {
-    alignItems: 'center',
-    backgroundColor: COLORS.dark.purple,
-    borderRadius: 20,
-    justifyContent: 'center',
-    marginBottom: 20,
-    padding: 20,
-  },
-  text: {
-    color: COLORS.dark.text,
-  },
-  balanceLabelText: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginBottom: 15,
-  },
-  selectedContactContainer: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingBottom: 10,
-    paddingHorizontal: 10,
-  },
-  selectedContactInfoContainer: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    gap: 15,
-  },
-  imageContainer: {
-    aspectRatio: 1,
-    width: 65,
-  },
-  image: {
-    aspectRatio: 1,
-    borderRadius: 100,
-    width: '100%',
-  },
-  selectedContactNameText: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginBottom: 8,
-  },
-  selectedContactCardInfoText: {
-    color: `${COLORS.dark.text}90`,
-    fontSize: 14,
-    fontWeight: '400',
-    letterSpacing: 0.2,
-  },
-  changeContactBtnText: {
-    color: COLORS.dark.yellow,
-    fontSize: 16,
-    fontWeight: '500',
   },
   amountInputContainer: {
     alignItems: 'center',
---
+++ components/send-money/TransferInfo.tsx
@@ -0,0 +1,123 @@
+import { WALLET_PATTERNS_BG } from '@/constants/assets.constants';
+import { COLORS } from '@/constants/colors.constants';
+import { DEFAULT_STYLES } from '@/constants/styles.constants';
+import { IContact } from '@/types';
+import { resolveSource } from '@/utils/assets.utils';
+import { Image, ImageBackground } from 'expo-image';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import WalletBalanceText from '../home/WalletBalanceText';
+
+type TransferInfoProps = {
+  selectedContact: IContact | null;
+  showChangeBtn?: boolean;
+};
+
+const TransferInfo = ({ selectedContact, showChangeBtn = false }: TransferInfoProps) => {
+  const router = useRouter();
+
+  const onChangeContact = () => {
+    router.back();
+  };
+
+  return (
+    <View style={styles.infoContainer}>
+      <ImageBackground style={styles.balanceContainer} source={WALLET_PATTERNS_BG}>
+        <Text style={[styles.text, styles.balanceLabelText]}>Wallet Balance</Text>
+        <WalletBalanceText balance={12890.0} />
+      </ImageBackground>
+      <View style={styles.selectedContactContainer}>
+        <View style={styles.selectedContactInfoContainer}>
+          <View style={styles.imageContainer}>
+            <Image source={resolveSource(selectedContact?.photo)} style={styles.image} />
+          </View>
+          <View>
+            <Text style={[styles.text, styles.selectedContactNameText]}>
+              {selectedContact?.firstName} {selectedContact?.lastName}
+            </Text>
+            <Text style={[styles.text, styles.selectedContactCardInfoText]}>
+              Payme - **** **** **** 9839
+            </Text>
+          </View>
+        </View>
+        {showChangeBtn && (
+          <Pressable
+            style={({ pressed }) => pressed && DEFAULT_STYLES.pressed}
+            onPress={onChangeContact}
+          >
+            <Text style={styles.changeContactBtnText}>Change</Text>
+          </Pressable>
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 2,
+  },
+  infoContainer: {
+    backgroundColor: COLORS.dark.secondary,
+    borderRadius: 30,
+    marginBottom: 20,
+    padding: 10,
+  },
+  balanceContainer: {
+    alignItems: 'center',
+    backgroundColor: COLORS.dark.purple,
+    borderRadius: 20,
+    justifyContent: 'center',
+    marginBottom: 20,
+    padding: 20,
+  },
+  text: {
+    color: COLORS.dark.text,
+  },
+  balanceLabelText: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 15,
+  },
+  selectedContactContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 10,
+    paddingHorizontal: 10,
+  },
+  selectedContactInfoContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 15,
+  },
+  imageContainer: {
+    aspectRatio: 1,
+    width: 65,
+  },
+  image: {
+    aspectRatio: 1,
+    borderRadius: 100,
+    width: '100%',
+  },
+  selectedContactNameText: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 8,
+  },
+  selectedContactCardInfoText: {
+    color: `${COLORS.dark.text}90`,
+    fontSize: 14,
+    fontWeight: '400',
+    letterSpacing: 0.2,
+  },
+  changeContactBtnText: {
+    color: COLORS.dark.yellow,
+    fontSize: 16,
+    fontWeight: '500',
+  },
+});
+
+export default TransferInfo;
